fd766e642f24fa6461d9f6e809183cb4ef7a9929
Victor Azevedo <josevcazevedo@gmail.com>
2023-03-25 17:22:48 -0300

    feat: add sign-in logic

---
+++ pages/api/sign-in.jsx
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxIiwibmFtZSI6IlZpY3RvciBBemV2ZWRvIiwiaWF0IjoxNTE2MjM5MDIyfQ.M4x7JPQepPHgnVlQ05sFBMiNSoOUn4cb6irni2A__QA",
+  });
+}
---
+++ src/services/token-service.js
@@ -0,0 +1,32 @@
+import nookies from "nookies";
+
+const ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
+
+const ONE_SECOND = 1;
+const ONE_MINUTE = ONE_SECOND * 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
+const ONE_MONTH = ONE_DAY * 30;
+const ONE_YEAR = ONE_DAY * 365;
+
+function save(accessToken, ctx = null) {
+  nookies.set(null, ACCESS_TOKEN_KEY, accessToken, {
+    maxAge: ONE_MONTH,
+    path: "/",
+  });
+}
+
+function get(ctx = null) {
+  const cookies = nookies.get(ctx);
+  return cookies[ACCESS_TOKEN_KEY] || "";
+}
+
+function destroy(ctx = null) {
+  nookies.destroy(ctx, ACCESS_TOKEN_KEY);
+}
+
+export const tokenService = {
+  save,
+  get,
+  destroy,
+};
