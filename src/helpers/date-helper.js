bc3eee2618a3fb5f095fa6bc8fdbd4fc8d716ae0
Victor Azevedo <josevcazevedo@gmail.com>
2023-03-29 15:58:14 -0300

    feat: add initial classe page

--- src/helpers/date-helper.js
+++ src/helpers/date-helper.js
@@ -1,9 +1,23 @@
 import dayjs from "dayjs";
 
+const weekDayPtBR = {
+  Sunday: "Domingo-Feira",
+  Monday: "Segunda-Feira",
+  Tuesday: "Terça-Feira",
+  Wednesday: "Quarta-Feira",
+  Thursday: "Quinta-Feira",
+  Friday: "Sexta-Feira",
+  Saturday: "Sábado-Feira",
+};
+
+export function weekDaysToPtBR(weekDays) {
+  return weekDays.map((weekDay) => weekDayPtBR[weekDay]);
+}
+
 export function getDayFromISOdate(ISOdate) {
   return dayjs(ISOdate).format("DD/MM/YYYY");
 }
 
 export function getTimeFromISOdate(ISOdate) {
-  return dayjs(ISOdate).format("HH:mm");
+  return dayjs(ISOdate).format("HH[h]mm[min]");
 }
