0f2646f62854452f525ed695fdb887a44542aba0
Victor Azevedo <josevcazevedo@gmail.com>
2023-03-30 19:06:28 -0300

    refactor: add toast and handleResponseError

--- src/hooks/api/useSignIn.jsx
+++ src/hooks/api/useSignIn.jsx
@@ -1,6 +1,3 @@
-import { useEffect } from "react";
-
-import handleResponseError from "@/src/errors/handleResponseError";
 import useAsync from "@/src/hooks/useAsync";
 import { authApi } from "@/src/services/api";
 
@@ -10,12 +7,6 @@ export default function useSignIn() {
     error: signInError,
     act: postSignIn,
   } = useAsync((data) => authApi.postSignIn(data), false);
-
-  useEffect(() => {
-    if (signInError) {
-      handleResponseError(signInError);
-    }
-  }, [signInError]);
 
   return {
     signInLoading,
