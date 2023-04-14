d9c7a3b9b61d2b1832f62183e23991f84418dcea
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-14 19:44:35 -0300

    feat: add /app/admin/courses/register

--- pages/app/admin/courses/register.jsx
+++ pages/app/admin/courses/register.jsx
@@ -1,5 +1,36 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Head from "next/head";
 
-export default function RegisterCourse() {
-  return <Typography>Cadastro</Typography>;
-}
+import { LayoutAdmin as DashboardLayout } from "src/layouts/dashboard/layout-admin";
+
+import CourseForm from "@/src/components/forms/CourseForm";
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <title>Cursos | Idioma 360</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Typography variant="h4" marginLeft={3}>
+              Cadastrar novo Curso
+            </Typography>
+            <CourseForm />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
---
+++ src/components/forms/CourseForm.jsx
@@ -0,0 +1,173 @@
+import handleResponseError from "@/src/errors/handleResponseError";
+import usePostCourse from "@/src/hooks/api/usePostCourse";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Unstable_Grid2 as Grid,
+  TextField,
+} from "@mui/material";
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+
+export default function CourseForm() {
+  const router = useRouter();
+
+  const { postCourse } = usePostCourse();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      creditHours: "",
+      imageUrl: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(3, "Deve possuir mais do que 3 caracteres")
+        .required("Nome é obrigatório"),
+      description: Yup.string()
+        .min(3, "Deve possuir mais do que 3 caracteres")
+        .required("Descrição é obrigatório"),
+      creditHours: Yup.number("Deve possuir ser número")
+        .min(0, "Deve ser maior que 0")
+        .required("Carga horária é obrigatória"),
+      imageUrl: Yup.string()
+        .url("Deve ser uma URL válida")
+        .required("Imagem é obrigatória"),
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        await postCourse(values);
+        toast.success("Curso cadastrado com sucesso");
+        router.push("/app/admin/courses");
+      } catch (err) {
+        handleResponseError(err);
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    },
+  });
+  return (
+    <form autoComplete="off" onSubmit={formik.handleSubmit} noValidate>
+      <Card>
+        <CardHeader title="Dados do curso:" />
+        <CardContent sx={{ pt: 0 }}>
+          <Box sx={{ m: -1.5 }}>
+            <Grid container spacing={3}>
+              <Grid xs={12} md={3}>
+                <TextField
+                  fullWidth
+                  error={!!(formik.touched.name && formik.errors.name)}
+                  helperText={formik.touched.name && formik.errors.name}
+                  label="Nome do Idioma"
+                  name="name"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="text"
+                  value={formik.values.name}
+                  id="name-input"
+                  required
+                />
+              </Grid>
+              <Grid xs={12} md={9}>
+                <TextField
+                  fullWidth
+                  error={
+                    !!(formik.touched.description && formik.errors.description)
+                  }
+                  helperText={
+                    formik.touched.description && formik.errors.description
+                  }
+                  label="Descrição"
+                  name="description"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="text"
+                  value={formik.values.description}
+                  id="description-input"
+                  required
+                />
+              </Grid>
+              <Grid xs={12} md={3}>
+                <TextField
+                  fullWidth
+                  error={
+                    !!(formik.touched.creditHours && formik.errors.creditHours)
+                  }
+                  helperText={
+                    formik.touched.creditHours && formik.errors.creditHours
+                  }
+                  label="Carga horária"
+                  name="creditHours"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="number"
+                  value={formik.values.creditHours}
+                  id="credit-hour-input"
+                  required
+                />
+              </Grid>
+              <Grid xs={12} md={9}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <TextField
+                    sx={{ width: 0.8 }}
+                    error={
+                      !!(formik.touched.imageUrl && formik.errors.imageUrl)
+                    }
+                    helperText={
+                      formik.touched.imageUrl && formik.errors.imageUrl
+                    }
+                    label="URL da imagem"
+                    name="imageUrl"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="text"
+                    value={formik.values.imageUrl}
+                    id="image-url-input"
+                    required
+                  />
+                  <Box
+                    sx={{
+                      flex: 1,
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {formik.values.imageUrl && !formik.errors.imageUrl && (
+                      <Avatar
+                        src={formik.values.imageUrl}
+                        alt="bandeira"
+                        sx={{ backgroundColor: "whites" }}
+                      ></Avatar>
+                    )}
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ justifyContent: "flex-end" }}>
+          <Button variant="contained" type="submit">
+            Salvar
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+}
