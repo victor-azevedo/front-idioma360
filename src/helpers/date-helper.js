84ff2be744c381108db05818573b507b5ef3e2b2
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-15 16:34:53 -0300

    feat: add parseDateToAPI

--- src/helpers/date-helper.js
+++ src/helpers/date-helper.js
@@ -25,3 +25,27 @@ export function getTimeFromISOdate(ISOdate) {
 export function isToday(ISOdate) {
   return getDayFromISOdate(ISOdate) === getDayFromISOdate();
 }
+
+export const parseDateTimeValuesToAPI = (object) => {
+  const DATE_KEYS = [
+    "birthday",
+    "startDate",
+    "endDate",
+    "testDate",
+    "resultDate",
+  ];
+  const TIME_KEYS = ["testStartTime", "testEndTime", "startTime", "endTime"];
+
+  DATE_KEYS.forEach((dateKey) => {
+    if (object[dateKey]) {
+      object[dateKey] = dayjs(object[dateKey]).format("YYYY-MM-DD");
+    }
+  });
+  TIME_KEYS.forEach((timeKey) => {
+    if (object[timeKey]) {
+      object[timeKey] = dayjs(object[timeKey]).format("HH:mm");
+    }
+  });
+
+  return object;
+};
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export * from "./cep-helper";
 export * from "./date-helper";
 export * from "./money-helper";
+export * from "./parse-offering-helper";
