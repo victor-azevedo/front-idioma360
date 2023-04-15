e953bb42b1815ac8f9c24da8af6d8062d0858433
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-15 09:43:00 -0300

    feat: add generic resource api service

---
+++ src/services/api/resources-api.js
@@ -0,0 +1,32 @@
+import { apiAuth } from "./api-service";
+
+export async function getAll({ resource }) {
+  const response = await apiAuth.get(`/${resource}`);
+
+  return response.data;
+}
+
+async function getById({ resource, id }) {
+  const response = await apiAuth.get(`/${resource}/${id}`);
+
+  return response.data;
+}
+
+async function patch({ resource, id, data }) {
+  const response = await apiAuth.patch(`/${resource}/${id}`, data);
+
+  return response.data;
+}
+
+async function deleteResource({ resource, id }) {
+  const response = await apiAuth.delete(`/${resource}/${id}`);
+
+  return response.data;
+}
+
+export const resourcesApi = {
+  getAll,
+  getById,
+  patch,
+  deleteResource,
+};
