56a2cfc2086291ce6100829f8304a639083c68f6
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-16 23:22:38 -0300

    feat: allow just edit or delete resource

--- src/components/AdminEditDeleteBox.jsx
+++ src/components/AdminEditDeleteBox.jsx
@@ -6,30 +6,41 @@ import { useState } from "react";
 
 import ConfirmDeleteDialog from "./ConfirmDeleteDialog";
 
-const AdminEditDeleteBox = ({ redirectEditRoute, handleDelete }) => {
+const AdminEditDeleteBox = ({
+  redirectEditRoute,
+  handleDelete,
+  onlyEdit,
+  onlyDelete,
+}) => {
   const router = useRouter();
 
   const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
 
   return (
     <Box sx={{ display: "flex", justifyContent: "center", gap: 3 }}>
-      <Button size="large" onClick={() => router.push(redirectEditRoute)}>
-        <EditNoteRoundedIcon />
-        Editar
-      </Button>
-      <Button
-        size="large"
-        onClick={() => setOpenConfirmDialog(true)}
-        color="warning"
-      >
-        <DeleteSweepRoundedIcon />
-        Deletar
-      </Button>
-      <ConfirmDeleteDialog
-        openConfirm={openConfirmDialog}
-        setOpenConfirm={setOpenConfirmDialog}
-        handleDelete={handleDelete}
-      />
+      {!onlyDelete && (
+        <Button size="large" onClick={() => router.push(redirectEditRoute)}>
+          <EditNoteRoundedIcon />
+          Editar
+        </Button>
+      )}
+      {!onlyEdit && (
+        <>
+          <Button
+            size="large"
+            onClick={() => setOpenConfirmDialog(true)}
+            color="warning"
+          >
+            <DeleteSweepRoundedIcon />
+            Deletar
+          </Button>
+          <ConfirmDeleteDialog
+            openConfirm={openConfirmDialog}
+            setOpenConfirm={setOpenConfirmDialog}
+            handleDelete={handleDelete}
+          />
+        </>
+      )}
     </Box>
   );
 };
