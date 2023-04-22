e388ac396bc5e510cfc461c2cf1f522a5a70e118
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-22 18:35:02 -0300

    feat: add theme transitions

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,14 +1,16 @@
-import { createTheme as createMuiTheme } from '@mui/material';
-import { createPalette } from './create-palette';
-import { createComponents } from './create-components';
-import { createShadows } from './create-shadows';
-import { createTypography } from './create-typography';
+import { createTheme as createMuiTheme } from "@mui/material";
+import { createComponents } from "./create-components";
+import { createPalette } from "./create-palette";
+import { createShadows } from "./create-shadows";
+import { createTransitions } from "./create-transitions";
+import { createTypography } from "./create-typography";
 
 export function createTheme() {
   const palette = createPalette();
   const components = createComponents({ palette });
   const shadows = createShadows();
   const typography = createTypography();
+  const transitions = createTransitions();
 
   return createMuiTheme({
     breakpoints: {
@@ -17,15 +19,16 @@ export function createTheme() {
         sm: 600,
         md: 900,
         lg: 1200,
-        xl: 1440
-      }
+        xl: 1440,
+      },
     },
     components,
     palette,
     shadows,
     shape: {
-      borderRadius: 8
+      borderRadius: 8,
     },
-    typography
+    typography,
+    transitions,
   });
 }
